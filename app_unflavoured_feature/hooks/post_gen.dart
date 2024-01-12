1b5cb5a94d35592f5eef6ff55ad7f0c00c1e9f7e
vberazategui@geocom.com.uy <vberazategui@geocom.com.uy>
2024-01-12 11:15:25 -0300

    fix: fixing progress log

--- app_unflavoured_feature/hooks/post_gen.dart
+++ app_unflavoured_feature/hooks/post_gen.dart
@@ -2,11 +2,15 @@ import 'dart:io';
 import 'package:mason/mason.dart';
 
 Future<void> run(HookContext context) async {
-  final progress = context.logger.progress('App Unflavoured Feature generated');
+  final progress = context.logger.progress();
 
-  progress('Running "dart fix --apply"');
+  progress.update('App Unflavoured Feature generated');
+
+  progress.update('Running "dart fix --apply"');
   await Process.run('fvm', ['dart', 'fix', '--apply']);
   await Process.run('fvm', ['flutter', 'format', '.']);
 
-  progress('Your App Unflavoured Feature is ready');
+  progress.update('Your App Unflavoured Feature is ready');
+
+  progress.complete();
 }
--- app_unflavoured_feature/hooks/pre_gen.dart
+++ app_unflavoured_feature/hooks/pre_gen.dart
@@ -5,7 +5,9 @@ import 'package:pubspec_parse/pubspec_parse.dart';
 import 'package:path/path.dart' as path;
 
 Future<void> run(HookContext context) async {
-  final progress = context.logger.progress('Getting package info');
+  final progress = context.logger.progress();
+
+  progress.update('Getting package info...');
 
   final pubspecContent = await File(
     path.join('..', 'pubspec.yaml'),
@@ -14,7 +16,8 @@ Future<void> run(HookContext context) async {
   final pubspec = Pubspec.parse(pubspecContent);
 
   context.vars = {...context.vars, 'package_name': pubspec.name};
-  progress.progress('Package name: ${pubspec.name} ');
+
+  progress.update('Package name is: ${pubspec.name} ');
 
   progress.complete();
 }
