13c98600b6c429e82002df831b1630c57f256583
vberazategui@geocom.com.uy <vberazategui@geocom.com.uy>
2024-02-09 12:21:02 -0300

    chore: add exceptions and failures

--- data_layer_package/__brick__/{{name.snakeCase()}}_data/lib/src/{{name.snakeCase()}}_data.dart
+++ data_layer_package/__brick__/{{name.snakeCase()}}_data/lib/src/{{name.snakeCase()}}_data.dart
@@ -1,5 +1,6 @@
 export 'api/api.dart';
 export 'datasources/datasources.dart';
+export 'execptions/execptions.dart';
 export 'extensions/extensions.dart';
 export 'models/models.dart';
 export 'repositories/repositories.dart';
