bd27d6989cfdba78176e1fbfff3ed046b3af3523
Vanessa Berazategui <vberazategui@geocom.com.uy>
2024-02-28 11:13:45 -0300

    fix: fix import path

--- domain_layer_entity/__brick__/{{name.snakeCase()}}.dart
+++ domain_layer_entity/__brick__/{{name.snakeCase()}}.dart
@@ -1,6 +1,6 @@
-import 'package:equatable/equatable.dart';
+import 'package:{{name.snakeCase()}}_domain/{{name.snakeCase()}}_domain.dart';
 
-class {{name.pascalCase()}} extends Equatable {
+class {{name.pascalCase()}} extends BaseEntity {
   const {{name.pascalCase()}}();
 
   @override
