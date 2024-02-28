0f3ac60e140d1ada75a718b1d3ab3d43b35ad8c1
Vanessa Berazategui <vberazategui@geocom.com.uy>
2024-02-28 11:27:50 -0300

    chore: update model gen code

--- data_layer_model/__brick__/{{name.snakeCase()}}_model.dart
+++ data_layer_model/__brick__/{{name.snakeCase()}}_model.dart
@@ -1,9 +1,11 @@
+import 'package:{{package_name}}_data/src/models/base/base_model.dart';
+import 'package:{{package_name}}_domain/billing_profile_domain.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part '{{name.snakeCase()}}_model.g.dart';
 
 @JsonSerializable(explicitToJson: true)
-class {{name.pascalCase()}}Model extends {{name.pascalCase()}} {
+class {{name.pascalCase()}}Model extends {{name.pascalCase()}} implements BaseModel {
   const {{name.pascalCase()}}Model();
 
   factory {{name.pascalCase()}}Model.fromJson(Map<String, dynamic> json) =>
